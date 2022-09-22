f63d42edc5c9149be196760224c95b23d8094892
Grzegorz <grzegorz@macbook-pro-grzegorz.home>
2022-09-22 19:01:02 +0200

    add conditional rendering

--- src/components/NewExpense/NewExpense.js
+++ src/components/NewExpense/NewExpense.js
@@ -1,8 +1,17 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './NewExpense.css'
 import ExpenseForm from './ExpenseForm';
 
 const NewExpense  = (props) => {
+
+    const [isEditing, setIsEditing] = useState(false);
+
+    const startEditingHandler = () => {
+        setIsEditing(true);
+    }
+    const stopEditingHandler = () => {
+        setIsEditing(false);
+    }
 
     const saveExpenseDataHandler = (enteredExpenseData) => {
         const expenseData = {
@@ -15,7 +24,8 @@ const NewExpense  = (props) => {
 
     return(
         <div className='new-expense'>
-<ExpenseForm onSaveExpenseData={saveExpenseDataHandler}/>
+            {!isEditing && <button onClick={startEditingHandler}>Add new expense</button>}
+            {isEditing && <ExpenseForm onSaveExpenseData={saveExpenseDataHandler} onCancel={stopEditingHandler}/>}
         </div>
     )
 }
